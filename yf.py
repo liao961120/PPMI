c625e193208251f1f92dd5d9789ed4dc49fd5138
liao <liao961120@gmail.com>
2020-02-21 23:28:47 +0800

    add platform indeppent settings

--- yf.py
+++ yf.py
@@ -1,11 +1,24 @@
-from os import system
+import platform
+import os
 from time import time
 
+
 def beep(start_time=None):
-    duration = 1.4  # seconds
-    freq = 400  # Hz
+    sys = platform.system()
+    duration = 1.6  # seconds
+    freq = 380      # Hz
+    
+    if sys == 'Linux' or sys == 'Darwin':
+        try:
+            os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
+        except:
+            print('Please install sox to enable beep sound!')
+    elif sys == 'Windows':
+        import winsound
+        winsound.Beep(freq, duration*1000)
+    else:
+        print('\007')
 
     if isinstance(start_time, float):
         print(f"Finished in {(time() - start_time)/60:.4} mins")
 
-    system('play -nq -t alsa synth {} sine {}'.format(duration, freq))

c7affe437eadcc0b66fded1c708e19f5c56f8aff
liao <liao961120@gmail.com>
2021-05-07 09:11:52 +0800

    update

--- export_plaintext.py
+++ export_plaintext.py
@@ -1,5 +1,6 @@
 #%%
 import re
+import tqdm
 from  zhon import hanzi
 from svd_embeddings import Embeddings
 
@@ -7,13 +8,13 @@ emb = Embeddings(embed_dim=50)
 pat = re.compile('[{}]'.format(hanzi.characters))
 
 outfile = []
-for w in emb.wi:
+for w in tqdm.tqdm(emb.wi):
     if len(w) == 0: continue
     if any( not pat.match(char) for char in w): 
         continue
     
     i = emb.wi[w]
-    vec = ' '.join(str(x) for x in emb.wordvec[i])
+    vec = '\t'.join(str(x) for x in emb.wordvec[i])
     outfile.append(f"{w}\t{vec}")
 
 with open("svd_ppmi_embeddings_50dim.txt", "w") as f:
